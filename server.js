95a884e6e580cb469126ff23ff1cca3c27979240
Steven Morgan <morgan.steven.dev@gmail.com>
2018-05-22 01:09:09 -0700

    Added contact links and navbar

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 //Business-specific values
 const BUSINESS_NAME = "Faith Whaley Massage";
+const PHONE_NUMBER = "123.456.7983";
+const EMAIL_ADDRESS = "faithwhaleymsg@test.com";
+const FACEBOOK = "http://www.facebook.com/test";
+const DEFAULT_PORT = 5000;
 
 const express = require('express');
 const app = express();
@@ -9,9 +13,9 @@ app.set('view engine', 'ejs');
 
 //Home page
 app.get('/', function (req, res) {
-    res.render('index', {B_N: BUSINESS_NAME});
+    res.render('index', {B_N: BUSINESS_NAME, P_N: PHONE_NUMBER, E_A: EMAIL_ADDRESS, FB: FACEBOOK });
 });
 
-app.listen(process.env.PORT || 5000, function () {
-    console.log("listening on port: " + process.env.PORT);
+app.listen(process.env.PORT || DEFAULT_PORT, function () {
+        console.log("listening on port: " + DEFAULT_PORT);
 });
