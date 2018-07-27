88747430f126615c1d4120f52f2b5e04d835a431
Steven Morgan <morgan.steven.dev@gmail.com>
2018-07-26 23:08:04 -0700

    placed click listener assignments inside for loop

--- public/js/content.js
+++ public/js/content.js
@@ -1,45 +1,37 @@
+"use strict";
 (() => {
-'use strict';
+    const TABS = [
+        $('#abt_tab'),
+        $('#loc_tab'),
+        $('#apnt_tab')
+    ];
 
-const TABS = [
-    $('#abt_tab'),
-    $('#loc_tab'),
-    $('#apnt_tab')
-];
+    const CONTENT = [
+        $('#about'),
+        $('#location'),
+        $('#appointment')
+    ];
 
-const CONTENT = [
-    $('#about'),
-    $('#location'),
-    $('#appointment')
-];
+    //hide all content except welcome message on initial load
+    $( document ).ready(function() {
+        toggle(CONTENT[0]);
+    });
 
-
-$( document ).ready(function() {
-    toggle(CONTENT[0]);
-
-});
-
-TABS[0].on("click", () => {
-    toggle(CONTENT[0]);
-});
-
-TABS[1].on("click", () => {
-    toggle(CONTENT[1]);
-});
-
-TABS[2].on("click", () => {
-    toggle(CONTENT[2]);
-});
-
-function toggle (div) {
-    // Hide all
-    hideall();
-    div.show();
-}
-
-function hideall () {
-    for (let i in CONTENT){
-        CONTENT[i].hide();
+    // Attach click listeners to tabs
+    for (let i in TABS){
+        TABS[i].on("click", () => {
+            toggle(CONTENT[i]);
+        });
     }
-}
+
+    function toggle (tab) {
+        hideall();
+        tab.show();
+    }
+
+    function hideall () {
+        for (let i in CONTENT){
+            CONTENT[i].hide();
+        }
+    }
 })();
